447b816997915e34abeb0bd4f97d3f7f528435e4
kotis <mkotamies@gmail.com>
2017-03-03 22:36:14 +0200

    Added animation when team scores

--- goalline/src/App.js
+++ goalline/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import classnames from 'classnames';
 
 class App extends Component {
 
@@ -17,13 +18,33 @@ class App extends Component {
     console.log("lol")
     return fetch("https://mw7qd5pah0.execute-api.us-east-1.amazonaws.com/dev/goal")
       .then(res => res.json())
-        .then(data => this.setState({data}))
+        .then(data => {
+          const team1Goal = this.goalsForTeam(data, "1") > this.goalsForTeam(this.state.data, "1")
+          const team2Goal = this.goalsForTeam(data, "2") > this.goalsForTeam(this.state.data, "2")
+          this.setState({data, team1Goal, team2Goal})
+        })
           .then(()=>setTimeout(this.callAPI.bind(this), 1000))
   }
-  render() {
 
-    const team1Goals = this.state.data.filter(goal => goal.player === "1").length % 10
-    const team2Goals = this.state.data.filter(goal => goal.player === "2").length % 10
+  goalsForTeam(data, team) {
+    return data.filter(goal => goal.player === team).length
+  }
+
+  render() {
+    const team1Goals = this.goalsForTeam(this.state.data, "1") 
+    const team2Goals = this.goalsForTeam(this.state.data, "2")
+
+    const team1Classes = classnames({
+      "teambox": true,
+      "team1": true,
+      "grows": this.state.team1Goal
+    })
+
+    const team2Classes = classnames({
+      "teambox": true,
+      "team2": true,
+      "grows": this.state.team2Goal
+    })
 
     return (
       <div className="App">
@@ -33,9 +54,9 @@ class App extends Component {
           <div className="country">Ruotsi</div>
         </div>
         <div className="container">
-          <div className="teambox team1">{team1Goals}</div>
+          <div className={team1Classes}>{team1Goals}</div>
           <div className="teambox">-</div>
-          <div className="teambox team2">{team2Goals}</div>
+          <div className={team2Classes}>{team2Goals}</div>
         </div>
       </div>
     );
