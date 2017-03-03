005d517d44f6c603a1e1ea45d394708cda0f2722
kotis <mkotamies@gmail.com>
2017-03-03 22:02:22 +0200

    First version of site

--- goalline/src/App.js
+++ goalline/src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount(){
+    this.callAPI()
+  }
+  callAPI(){
+    console.log("lol")
+    return fetch("https://mw7qd5pah0.execute-api.us-east-1.amazonaws.com/dev/goal")
+      .then(res => res.json())
+        .then(data => this.setState({data}))
+          .then(()=>setTimeout(this.callAPI.bind(this), 1000))
+  }
   render() {
+
+    const team1Goals = this.state.data.filter(goal => goal.player === "1").length % 10
+    const team2Goals = this.state.data.filter(goal => goal.player === "2").length % 10
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <h2>Pöytäfutiksen tilanne</h2>
+        <div className="container">
+          <div className="country">Suomi</div>
+          <div className="country">Ruotsi</div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="container">
+          <div className="teambox team1">{team1Goals}</div>
+          <div className="teambox">-</div>
+          <div className="teambox team2">{team2Goals}</div>
+        </div>
       </div>
     );
   }
