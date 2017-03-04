38ff9bc0d45e1365b71c04b6944de0f1e69eabcc
Klaus Julin <klaus.julin@wunderdog.fi>
2017-03-04 20:16:28 +0200

    Adds goal sound and game timer

--- goalline/src/App.js
+++ goalline/src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
 import './App.css'
 import classnames from 'classnames'
+import Stopwatch from 'timer-stopwatch'
 
 import Api from './Api'
+
+const goalSound = new Audio('goalshort.mp3')
 
 class App extends Component {
 
@@ -10,13 +13,17 @@ class App extends Component {
     super(props)
     this.state = {
       data: [],
-      status: false
+      status: false,
+      team1: localStorage.getItem('goalline.team1.name') || 'Suomi',
+      team2: localStorage.getItem('goalline.team2.name') || 'Ruotsi',
+      time: 0
     }
   }
 
   componentDidMount(){
     this.getCurrentScore(false)
     this.getGameStatus()
+    this.startGame()
   }
 
   getCurrentScore(shouldAnimateChange) {
@@ -35,22 +42,37 @@ class App extends Component {
   }
 
   postpone(func) {
-    return () => setTimeout(func.bind(this), 1000)
+    return () => setTimeout(func.bind(this), 500)
   }
 
   restartGame() {
     return Api.restartGame()
       .then(this.updateScores.bind(this))
+      .then(this.stateGame.bind(this))
+  }
+
+  startGame() {
+    const timer = new Stopwatch()
+    timer.onTime(time => this.setState({time: time.ms}))
+    timer.start()
   }
 
   updateScores(data, shouldAnimateChange = true) {
     const team1Goal = shouldAnimateChange && this.goalsForTeam(data, "1") > this.goalsForTeam(this.state.data, "1")
     const team2Goal = shouldAnimateChange && this.goalsForTeam(data, "2") > this.goalsForTeam(this.state.data, "2")
+
     this.setState({data, team1Goal, team2Goal})
   }
 
   goalsForTeam(data, team) {
     return data.filter(goal => goal.player === team).length
+  }
+
+  updateTeamName(team, newName) {
+    localStorage.setItem(`goalline.${team}.name`, newName)
+    const state = {}
+    state[team] = newName
+    this.setState(state)
   }
 
   render() {
@@ -71,12 +93,16 @@ class App extends Component {
       "grows": this.state.team2Goal
     })
 
+    if(this.state.team1Goal || this.state.team2Goal) goalSound.play()
+
     return (
       <div className="App">
+
         <h2>Pöytäfutiksen tilanne</h2>
+        <span className="timer">{this.renderTime()}</span>
         <div className="container teams">
-          <span>Suomi</span>
-          <span>Ruotsi</span>
+          <input type="text" value={this.state.team1} onChange={e => this.updateTeamName('team1', e.target.value)} />
+          <input type="text" value={this.state.team2} onChange={e => this.updateTeamName('team2', e.target.value)} />
         </div>
         <div className="container score">
           <div className={team1Classes}>{team1Goals}</div>
@@ -88,6 +114,17 @@ class App extends Component {
         <h2 className="restart" onClick={this.restartGame.bind(this)}>Uusi peli</h2>
       </div>
     );
+  }
+
+  renderTime() {
+    const time = this.state.time
+    let minutes = parseInt(time / (60 * 1000))
+    let seconds = parseInt((time % (60 * 1000)) / 1000)
+
+    if(minutes < 10) minutes = "0" + minutes
+    if(seconds < 10) seconds =  "0" + seconds
+
+    return `${minutes}:${seconds}`
   }
 }
 
