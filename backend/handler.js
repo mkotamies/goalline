a4402ad97351d8e79e157cb9fa52651773a2cb98
Klaus Julin <klaus.julin@wunderdog.fi>
2017-03-18 13:54:59 +0200

    Initial implementation for sending game score to twitter

--- backend/handler.js
+++ backend/handler.js
@@ -3,6 +3,14 @@ import querystring from 'querystring'
 import Promise from 'bluebird'
 import R from 'ramda'
 import {dynamo} from './common/dynamodb'
+import twitter from 'twitter'
+
+const twitterClient = new Twitter({
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
+})
 
 Promise.promisifyAll(dynamo)
 
@@ -17,16 +25,48 @@ function loadGoals() {
     .then(result => result.Items)
 }
 
+function sendScoreToTwitter(ply1Goals, ply2Goals) {
+  const ply1Name = 'Suomi'
+  const ply2Name = 'Ruotsi'
+
+  let tweet
+
+  if(ply1Goals > ply1Goals) {
+    tweet = `${ply1Name} beats ${ply2Name} ${ply1Goals} - ${ply2Goals}`
+  } else {
+    tweet = `${ply2Name} beats ${ply1Name} ${ply2Goals} - ${ply1Goals}`
+  }
+
+  return twitterClient.post('statuses/update', {status: tweet})
+    .then(console.log, console.log)
+}
+
 export function addGoal(req, context, cb) {
   console.log(req.body)
   const time = moment()
   const player = (req.body && req.body.player) || 'none'
 
+  const checkIfGameEnded = () => {
+
+    loadGoals()
+      .then(result => result.Items)
+      .then(goals => {
+        const ply1Goals = goals.filter(goal => goal.player === "1").sum
+        const ply2Goals = goals.filter(goal => goal.player === "2").sum
+
+        if((ply1Goals === 10 && ply2Goals < 10) || (ply2Goals === 10 && ply1Goals < 10)) {
+         return sendScoreToTwitter(ply1Goals, ply2Goals)
+        }
+        else return null
+      })
+      .then(() => cb(null, {message: `Goal Saved for player ${player}! Time is ${time.toString()}`}))
+  }
+
   dynamo.putItem(
     {TableName: 'goals', Item: {player, time: time.valueOf(), humanTime: time.toString()}},
     function(err, result) {
       if(err) cb(err)
-      else cb(null, {message: `Goal Saved for player ${player}! Time is ${time.toString()}`})
+      else checkIfGameEnded()
     }
   )
 }
